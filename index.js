2ed3a5ff4e9fda3709a2d8f7f9c970b464ab3058
Arslaan123Iqbal <arslaniqbalmgt@gmail.con>
2024-03-11 16:25:10 +0500

    first ec2 commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express"
+
+const app = express();
+
+app.listen(5000, (req, res) =>{
+    console.log('lisitnineif on pirt 5000')
+})
+
+app.get('/', (req, res) =>{
+    res.json('testing ec2 app')
+})
